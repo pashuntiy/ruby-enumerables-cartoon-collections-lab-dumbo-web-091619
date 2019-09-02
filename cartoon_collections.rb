01678f4f71ceac48c76b38475a704ee7eabef9d5
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-02 19:17:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map{ |x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  short_words.any? do |x| x.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
