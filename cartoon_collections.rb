29d5956b0262bb6771c7d3ec1410dbf75492f9a0
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-02 18:53:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.map.with_index { |x, i| x = (i+1).to_s + (". "+ x) 
+  puts x}
+  array
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map{ |x| x.capitalize + "!"}
 end
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
